8f063119a3fd6472ea00da23e9ab7c5e3b85c483
Fabrice Agu <f.agu@oodrive.com>
2019-08-08 17:56:25 +0200

    fix url

--- recreatedb.bat
+++ recreatedb.bat
@@ -8,6 +8,7 @@ del src\Migrations\Version2019*.*
 php bin/console make:migration
 php bin/console doctrine:migrations:migrate --no-interaction
 php bin/console doctrine:fixtures:load --no-interaction --group=MenuItemFixtures
+php bin/console cache:clear
 
 echo.
 echo To add fake values :
